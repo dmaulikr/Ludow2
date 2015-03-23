081cc9a4a3034a30663b8509cb495f0ee78eb019
arnaudJacquez <jacquez.arnaud@gmail.com>
2015-03-23 08:34:32 +0100

    Initial Commit

---
+++ Ludow2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ludow2
+//
+//  Created by JACQUEZ on 23/03/2015.
+//  Copyright (c) 2015 JACQUEZ_ESTREM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Ludow2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Ludow2
+//
+//  Created by JACQUEZ on 23/03/2015.
+//  Copyright (c) 2015 JACQUEZ_ESTREM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
